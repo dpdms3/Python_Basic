b6cbf02ec9c8a5824ff414ca3085cad566415937
dpdms3 <133951371+dpdms3@users.noreply.github.com>
2023-09-15 12:14:27 +0900

    Update Python_01_variable.py
    Finished

--- lecture/python_01_variable.py
+++ lecture/python_01_variable.py
@@ -108,6 +108,88 @@ print(num)  # 출력: 10
 # 대입연산자(=) : 우측의 값을 좌측에 저장
 # 동등연산자(==) : Equal
 
+# name 변수 생성
+# "Ye Eun" 값을 name 변수에 담기
+
+# name(변수명), =(대입연산자), "Ye Eun"(값)
+name = "Ye Eun"
+
+# 명명규칙(Naming Rule)
+# * 변수, 함수, 클래스 등의 사용자 정의 이름을 붙일 때 사용!
+# * 명확하고 알아보기 쉽게 짓기!
+#
+#  1.영문 대소문자, 숫자, 특수문자(_)만 사용
+#  2.숫자로 시작할 수 없음
+#    abc1(O), 1abc(X)
+#  3.영어 대소문자 구별
+#    abc Abc ABc, abC 모두 다른 변수
+#  4.예약어 사용 불가
+#    예약어: Python에서 미리 선정하여 사용중인 키워드
+#           ex) print, for, while, if, else, class, try, except, True, False
+#               and, return, import, def, pass, break, continue, del, ...
+# Naming Method
+# - 변수, 함수, 클래스 등의 사용자 정의 이름에 사용하는 기법
+# - 프로그래밍 언어별로 사용하는 Naming Method가 다름!
+
+#  1.snake_case: 소문자만 사용, 합성어는 (_)사용
+#    ex) student_name
+#  2.CamelCase: 첫글자 소문자, 합성어 첫글자 대문자
+#    ex) studentName
+#  3.PascalCase: 첫글자 대문자, 합성어 첫글자 대문자
+#    ex) StudentName
+
+#                변수             함수            클래스
+# Java, C      camelCase       camelCase()      PascalCase
+# Python       snake_case      snake_case()     PascalCase
+# 시험에 나올 수 있음
+
+# 동적 출력!
+print("=" * 200)
+student_num = 20233138
+student_name = "Ye Eun"
+
+# 출력 예: 조선대학교 20233138, Ye Eun 입니다.
+print("조선대학교 20233138, Ye Eun 입니다.")  # 하드 코딩 지양!
+
+#  1.format() 함수 - Old
+print("조선대학교 {}, {} 입니다.".format(student_num, student_name))
+
+#  2.f-string - New
+print(f"조선대학교 {student_num}, {student_name} 입니다.")
+
+# 간단한 사칙연산
+# + : 더하기
+# - : 빼기
+# * : 곱하기
+# / : 나누기
+# // : 나누기(몫)
+# % : 나누기(나머지)
+# ** or ^ : 제곱
+#
+# 5/2 : 나누기           2.5
+# 5//2 : 나누기(몫)      2
+# 5%2 : 나누기(나머지)   1
+
+# 문제?
+num = 9
+num - 1
+num + 2
+print(num)
 
 
 
+
+
+# 개발자: Web(프론트엔드, 백엔드), 앱(Android, Apple), 웹 퍼블리셔
+# 웹 디자이너
+# 서버 엔지니어(Linux 운영 관리) + 클라우드 개발자
+# 네트워크 엔지니어
+# 테이터베이스 엔지니어, 데이터베이스 관리자
+# SQL 튜너
+# 데이터 모델러
+# ERP 개발자
+# 보안 개발자
+# 인공지능, 데이터분석가, 데이터 사이언티스트, 프롬프트 엔지니어
+# 데이터 엔지니어
+
+#
